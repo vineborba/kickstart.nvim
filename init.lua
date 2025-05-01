e4c5405a6af7c88a6d7ead9651ad914b74f37488
Vinicius de Borba <vinicius.borba@softdesign.com.br>
2025-04-30 23:26:57 -0300

    python: added basic support

--- init.lua
+++ init.lua
@@ -665,7 +665,8 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {}, -- python
+        taplo = {}, -- toml
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
