8da139ca94463f3b97655e7bcaf7c5f02316de03
Vinicius de Borba <vinicius.borba@hotmail.com>
2025-07-22 20:31:52 -0300

    add keybind to open diagnostic window

--- init.lua
+++ init.lua
@@ -122,6 +122,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float, { desc = 'Open diagnostic float' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
